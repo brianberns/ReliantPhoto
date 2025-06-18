3c73e4e3ece1b44d8b3a3d226ba03ce20b88ed2e
Brian Berns <brianberns@gmail.com>
2025-06-18 17:47:10 -0400

    Add project files.

---
+++ Location.fs
@@ -0,0 +1,56 @@
+﻿namespace Reliant.Photo
+
+open System
+open System.Diagnostics
+open System.IO
+
+open Avalonia.Controls
+open Avalonia.Layout
+open Avalonia.Media.Imaging
+open Avalonia.FuncUI.DSL
+    
+type LocationState =
+    {
+        DirectoryInfo : DirectoryInfo
+    }
+
+type LocationMessage =
+    | NoOp
+
+module Location =
+
+    let init path =
+        {
+            DirectoryInfo =
+                Environment.SpecialFolder.MyPictures
+                    |> Environment.GetFolderPath
+                    |> DirectoryInfo
+        }
+
+    let update msg state =
+        match msg with
+            | NoOp -> state
+
+    let tryLoadBitmap (fileInfo : FileInfo) =
+        let path = fileInfo.FullName
+        try
+            new Bitmap(path) |> Some
+        with exn ->
+            Trace.WriteLine($"{path}: {exn.Message}")
+            None
+
+    let view state dispatch =
+
+        let bitmaps =
+            state.DirectoryInfo
+                .GetFiles()
+                |> Array.choose tryLoadBitmap
+                    
+        DockPanel.create [
+            DockPanel.children [
+                for bitmap in bitmaps do
+                    Image.create [
+                        Image.source bitmap
+                    ]
+            ]
+        ]
---
+++ Program.fs
@@ -0,0 +1,45 @@
+﻿namespace Reliant.Photo
+
+open Avalonia
+open Avalonia.Controls.ApplicationLifetimes
+open Avalonia.FuncUI.Elmish
+open Avalonia.FuncUI.Hosts
+open Avalonia.Themes.Fluent
+
+open Elmish
+
+type MainWindow() as this =
+    inherit HostWindow(
+        Title = "Reliant Photo",
+        Width = 400.0,
+        Height = 400.0)
+    do
+        Elmish.Program.mkSimple
+            Location.init
+            Location.update
+            Location.view
+            |> Program.withHost this
+            |> Program.run
+
+type App() =
+    inherit Application()
+
+    override this.Initialize() =
+        this.Styles.Add (FluentTheme())
+        this.RequestedThemeVariant <- Styling.ThemeVariant.Dark
+
+    override this.OnFrameworkInitializationCompleted() =
+        match this.ApplicationLifetime with
+            | :? IClassicDesktopStyleApplicationLifetime as lifetime ->
+                lifetime.MainWindow <- MainWindow()
+            | _ -> ()
+
+module Program =
+
+    [<EntryPoint>]
+    let main args =
+        AppBuilder
+            .Configure<App>()
+            .UsePlatformDetect()
+            .UseSkia()
+            .StartWithClassicDesktopLifetime(args)
