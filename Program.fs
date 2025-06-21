a742ec8fd098ab669ec92e64d9a50c853ef8f3e2
Brian Berns <brianberns@gmail.com>
2025-06-21 11:20:00 -0400

    Polish.

--- Program.fs
+++ Program.fs
@@ -15,16 +15,7 @@ open Avalonia.Themes.Fluent
 type MainWindow(args : _[]) as this =
     inherit HostWindow(Title = "Reliant Photo")
 
-    let onClosing _ =
-        Settings.save {
-            Left = this.Position.X
-            Top = this.Position.Y
-            Width = this.Width
-            Height = this.Height
-            Maximized = this.WindowState = WindowState.Maximized
-        }
-
-    do
+    let loadSettings _ =
         Settings.tryLoad ()
             |> Option.iter (fun settings ->
             if settings.Maximized then
@@ -35,24 +26,37 @@ type MainWindow(args : _[]) as this =
                 this.Width <- settings.Width
                 this.Height <- settings.Height)
 
-        let path =
-            if args.Length > 0 then
-                FileInfo(args[0])
-            else
-                let dirInfo =
-                    Environment.SpecialFolder.MyPictures
-                        |> Environment.GetFolderPath
-                        |> DirectoryInfo
-                dirInfo.GetFiles()[0]
+    let saveSettings _ =
+        Settings.save {
+            Left = this.Position.X
+            Top = this.Position.Y
+            Width = this.Width
+            Height = this.Height
+            Maximized = this.WindowState = WindowState.Maximized
+        }
 
-        this.Closing.Add(onClosing)
+    let getInitialPath (args : _[]) =
+        if args.Length > 0 then
+            FileInfo(args[0])
+        else
+            let dirInfo =
+                Environment.SpecialFolder.MyPictures
+                    |> Environment.GetFolderPath
+                    |> DirectoryInfo
+            dirInfo.GetFiles()[0]
 
+    let run path =
         Elmish.Program.mkProgram
             Image.init
             Image.update
             Image.view
             |> Program.withHost this
             |> Program.runWith path
+
+    do
+        loadSettings ()
+        this.Closing.Add(saveSettings)
+        getInitialPath args |> run
 
 type App() =
     inherit Application()
