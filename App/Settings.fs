4f7dc766069e67314ca0644ea779020fd8f3c245
Brian Berns <brianberns@gmail.com>
2025-06-25 11:40:57 -0400

    Polish.

--- App/Settings.fs
+++ App/Settings.fs
@@ -5,17 +5,28 @@ open System.Diagnostics
 open System.IO
 open System.Text.Json
 
+/// Persistent user settings.
 type Settings =
     {
+        /// X-coordinate of window.
         Left : int
+
+        /// Y-coordinate of window.
         Top : int
+
+        /// Width of window.
         Width : double
+
+        /// Height of window.
         Height : double
+
+        /// Window is maximized?
         Maximized : bool
     }
 
 module Settings =
 
+    /// File in which settings are stored.
     let private settingsFile =
         Path.Combine(
             Environment.GetFolderPath(
@@ -24,9 +35,11 @@ module Settings =
             "Settings.json")
             |> FileInfo
 
+    /// Serialization options.
     let private options =
         JsonSerializerOptions(WriteIndented = true)
 
+    /// Saves the given settings.
     let save (settings : Settings) =
         try
             let json = JsonSerializer.Serialize(settings, options)
@@ -35,14 +48,15 @@ module Settings =
         with exn ->
             Trace.WriteLine($"Could not save settings: {exn.Message}")
 
+    /// Tries to load settings from a file.
     let tryLoad () =
         try
             if settingsFile.Exists then
-                let json = File.ReadAllText(settingsFile.FullName)
-                JsonSerializer.Deserialize<Settings>(json)
+                settingsFile.FullName
+                    |> File.ReadAllText
+                    |> JsonSerializer.Deserialize<Settings>
                     |> Some
-            else
-                None
+            else None
         with exn ->
             Trace.WriteLine($"Could not load settings: {exn.Message}")
             None
