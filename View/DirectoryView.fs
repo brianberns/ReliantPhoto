d3f50d87c06effacd77a6c0d686793469c316110
Brian Berns <brianberns@gmail.com>
2025-06-25 08:12:55 -0400

    Polish.

--- View/DirectoryView.fs
+++ View/DirectoryView.fs
@@ -14,39 +14,44 @@ module Cursor =
 
 module DirectoryView =
 
+    /// Creates an image control.
+    let private createImage file (source : IImage) dispatch =
+        Image.create [
+            Image.source source
+            Image.height source.Size.Height   // why is this necessary?
+            Image.stretch Stretch.Uniform
+            Image.margin 8.0
+            Image.onTapped (fun _ ->
+                dispatch (SwitchToImage file))
+        ]
+
     /// Creates a view of the given model.
     let view (model : DirectoryModel) dispatch =
         DockPanel.create [
+
             if model.IsLoading then
                 DockPanel.cursor Cursor.wait
                 DockPanel.background "Transparent"   // needed to force the cursor change for some reason
-            else
 
-                let images =
-                    [
-                        for file, result in model.ImageLoadPairs do
-                            match result with
-                                | Ok image ->
-                                    Image.create [
-                                        Image.source image
-                                        Image.height image.Size.Height   // why is this necessary?
-                                        Image.stretch Stretch.Uniform
-                                        Image.margin 8.0
-                                        Image.onTapped (fun _ ->
-                                            dispatch (SwitchToImage file))
-                                    ] :> IView
-                                | Error _ -> ()
-                    ]
+            let images =
+                [
+                    for file, result in model.ImageLoadPairs do
+                        match result with
+                            | Ok source ->
+                                createImage file source dispatch
+                                    :> IView
+                            | Error _ -> ()
+                ]
 
-                DockPanel.children [
-                    ScrollViewer.create [
-                        ScrollViewer.content (
-                            WrapPanel.create [
-                                WrapPanel.orientation Orientation.Horizontal
-                                WrapPanel.margin 8.0
-                                WrapPanel.children images
-                            ]
-                        )
-                    ]
+            DockPanel.children [
+                ScrollViewer.create [
+                    ScrollViewer.content (
+                        WrapPanel.create [
+                            WrapPanel.orientation Orientation.Horizontal
+                            WrapPanel.margin 8.0
+                            WrapPanel.children images
+                        ]
+                    )
+                ]
             ]
         ]
