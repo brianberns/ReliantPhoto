bf2cc32f4c89184dcf29822013513e0c090f8bd2
Brian Berns <brianberns@gmail.com>
2025-06-25 07:58:03 -0400

    Polish.

--- Model/ImageFile.fs
+++ Model/ImageFile.fs
@@ -8,7 +8,8 @@ open SixLabors.ImageSharp
 open SixLabors.ImageSharp.Formats
 open SixLabors.ImageSharp.Formats.Png
 
-type ImageResult = Result<Bitmap, string>
+/// Result of trying to load an image.
+type ImageResult = Result<Bitmap, string (*error message*)>
 
 module ImageFile =
 
@@ -18,28 +19,37 @@ module ImageFile =
             CompressionLevel =
                 PngCompressionLevel.NoCompression)
 
+    /// Creates decoder options for loading an image.
+    let private getDecoderOptions targetHeightOpt (file : FileInfo) =
+        match targetHeightOpt with
+
+                // decode to the given height
+            | Some targetHeight ->
+                let targetWidth =
+                    let imgInfo = Image.Identify(file.FullName)
+                    int (float imgInfo.Width
+                        / float imgInfo.Height * float targetHeight)
+                DecoderOptions(
+                    TargetSize = Size(targetWidth, targetHeight))
+
+            | None -> DecoderOptions()
+
     /// Tries to load an image from the given file.
-    let tryLoadImage targetHeightOpt (file : FileInfo) =
+    let tryLoadImage targetHeightOpt file =
         async {
             try
                     // load image to PNG format
                 use stream = new MemoryStream()
                 do
                     let options =
-                        match targetHeightOpt with
-                            | Some targetHeight ->
-                                let imgInfo = Image.Identify(file.FullName)
-                                let targetWidth =
-                                    int (float imgInfo.Width
-                                        / float imgInfo.Height * float targetHeight)
-                                DecoderOptions(TargetSize = Size(targetWidth, targetHeight))
-                            | None -> DecoderOptions()
+                        getDecoderOptions targetHeightOpt file
                     use image = Image.Load(options, file.FullName)
                     image.SaveAsPng(stream, pngEncoder)
                     stream.Position <- 0
 
                     // create Avalonia bitmap
-                return (Ok (new Bitmap(stream)) : ImageResult)
+                let image = new Bitmap(stream)
+                return (Ok image : ImageResult)
 
             with exn ->
                 return Error exn.Message
