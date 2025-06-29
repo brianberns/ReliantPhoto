18594aeae90a62e6692fdc7f8c4232f4c81b24c3
Brian Berns <brianberns@gmail.com>
2025-06-29 12:15:18 -0400

    Try native Avalonia image loader first.

--- Model/ImageFile.fs
+++ Model/ImageFile.fs
@@ -4,17 +4,18 @@ open System.IO
 
 open Avalonia.Media.Imaging
 
-open SixLabors.ImageSharp
-open SixLabors.ImageSharp.Formats
-open SixLabors.ImageSharp.Formats.Png
-
 /// Result of trying to load an image.
 type ImageResult = Result<Bitmap, string (*error message*)>
 
 /// An image result for a specific file.
 type FileImageResult = FileInfo * ImageResult
 
-module ImageFile =
+/// SixLabors supports TIFF, while Avalonia doesn't.
+module private SixLabors =
+
+    open SixLabors.ImageSharp
+    open SixLabors.ImageSharp.Formats
+    open SixLabors.ImageSharp.Formats.Png
 
     /// PNG encoder.
     let private pngEncoder =
@@ -37,23 +38,43 @@ module ImageFile =
 
             | None -> DecoderOptions()
 
+    /// Loads an image from the given file.
+    let loadImage targetHeightOpt file =
+
+            // load image to PNG format
+        use stream = new MemoryStream()
+        do
+            let options =
+                getDecoderOptions targetHeightOpt file
+            use image = Image.Load(options, file.FullName)
+            image.SaveAsPng(stream, pngEncoder)
+            stream.Position <- 0
+
+            // create Avalonia bitmap
+        new Bitmap(stream)
+
+module ImageFile =
+
+    /// Loads an image from the given file.
+    let private loadImage targetHeightOpt (file : FileInfo) =
+
+        try
+            use stream = file.OpenRead()
+            match targetHeightOpt with
+                | Some height ->
+                    Bitmap.DecodeToHeight(stream, height)
+                | None -> new Bitmap(stream)
+
+            // try SixLabors for images not supported by Avalonia
+        with _ ->
+            SixLabors.loadImage targetHeightOpt file
+
     /// Tries to load an image from the given file.
     let tryLoadImage targetHeightOpt file =
         async {
             try
-                    // load image to PNG format
-                use stream = new MemoryStream()
-                do
-                    let options =
-                        getDecoderOptions targetHeightOpt file
-                    use image = Image.Load(options, file.FullName)
-                    image.SaveAsPng(stream, pngEncoder)
-                    stream.Position <- 0
-
-                    // create Avalonia bitmap
-                let image = new Bitmap(stream)
+                let image = loadImage targetHeightOpt file
                 return (Ok image : ImageResult)
-
             with exn ->
                 return Error exn.Message
         }
