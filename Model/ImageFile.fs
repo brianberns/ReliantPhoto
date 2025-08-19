2772a6318a85eb85fe2ace882523cebdcb353cd4
Brian Berns <brianberns@gmail.com>
2025-08-19 14:44:08 -0400

    Modularized.

--- Model/ImageFile.fs
+++ Model/ImageFile.fs
@@ -29,100 +29,16 @@ module FileInfo =
 
         loop 100
 
-/// SixLabors supports TIFF, while Avalonia doesn't.
-module private SixLabors =
-
-    open SixLabors.ImageSharp
-    open SixLabors.ImageSharp.Processing
-    open SixLabors.ImageSharp.Formats
-    open SixLabors.ImageSharp.Formats.Png
-    open SixLabors.ImageSharp.Metadata.Profiles.Exif
-
-    /// Gets the orientation of the given image.
-    let getOrientation (imageInfo : ImageInfo) =
-        option {
-            let! profile =
-                Option.ofObj imageInfo.Metadata.ExifProfile
-            let flag, exifValue =
-                profile.TryGetValue(ExifTag.Orientation)
-            if flag then
-                return exifValue.Value
-        } |> Option.defaultValue ExifOrientationMode.TopLeft
-
-    /// Gets the target size for the given target height,
-    /// accounting for rotation, if necessary.
-    let getTargetSize targetHeight imageInfo =
-        match getOrientation imageInfo with
-            | ExifOrientationMode.LeftTop
-            | ExifOrientationMode.RightTop
-            | ExifOrientationMode.RightBottom
-            | ExifOrientationMode.LeftBottom ->
-                Size(targetHeight, 0)   // rotate
-            | _ ->
-                Size(0, targetHeight)
-
-    /// PNG encoder.
-    let private pngEncoder =
-        PngEncoder(
-            CompressionLevel =
-                PngCompressionLevel.NoCompression)
-
-    /// Loads an image from the given file.
-    let private loadImageImpl options (file : FileInfo) =
-
-        use stream = new MemoryStream()
-        do
-                // load image
-            use image = Image.Load(options, file.FullName)
-            image.Mutate(_.AutoOrient() >> ignore)
-
-                // re-encode image to stream
-            let encoder =
-                match image.Metadata.DecodedImageFormat.Name.ToLower() with
-                    | "tiff" -> pngEncoder :> IImageEncoder
-                    | _ ->
-                        image.Configuration
-                            .ImageFormatsManager
-                            .GetEncoder(
-                                image.Metadata.DecodedImageFormat)
-            image.Save(stream, encoder)
-            stream.Position <- 0
-
-            // create Avalonia bitmap
-        new Bitmap(stream)
-
-    /// Loads an image from the given file.
-    let loadImage file =
-        let options = DecoderOptions()
-        loadImageImpl options file
-
-    /// Loads a thumbnail image from the given file.
-    let loadThumbnail height (file : FileInfo) =
-        let imageInfo = Image.Identify file.FullName
-        let options =
-            DecoderOptions(
-                TargetSize =   // decode to the given height
-                    getTargetSize height imageInfo)
-        loadImageImpl options file
-
 /// An image result for a specific file.
 type FileImageResult = FileInfo * Result<Bitmap, string (*error message*)>
 
 module ImageFile =
 
-    /// Loads an image from the given file.
-    let private loadImage file =
-        SixLabors.loadImage file
-
-    /// Loads a thumbnail image from the given file.
-    let private loadThumbnail height file =
-        SixLabors.loadThumbnail height file
-
     /// Tries to load an image from the given file.
     let tryLoadImage file =
         async {
             try
-                let image = loadImage file
+                let image = ImageSharp.loadImage file
                 return Ok image
             with exn ->
                 return Error exn.Message
@@ -132,7 +48,7 @@ module ImageFile =
     let tryLoadThumbnail height file =
         async {
             try
-                let thumbnail = loadThumbnail height file
+                let thumbnail = ImageSharp.loadThumbnail height file
                 return Ok thumbnail
             with exn ->
                 return Error exn.Message
---
+++ Model/ImageSharp.fs
@@ -0,0 +1,80 @@
+﻿namespace Reliant.Photo
+
+open System.IO
+
+open Avalonia.Media.Imaging
+
+open SixLabors.ImageSharp
+open SixLabors.ImageSharp.Processing
+open SixLabors.ImageSharp.Formats
+open SixLabors.ImageSharp.Formats.Png
+open SixLabors.ImageSharp.Metadata.Profiles.Exif
+
+module private ImageSharp =
+
+    /// Gets the orientation of the given image.
+    let getOrientation (imageInfo : ImageInfo) =
+        option {
+            let! profile =
+                Option.ofObj imageInfo.Metadata.ExifProfile
+            let flag, exifValue =
+                profile.TryGetValue(ExifTag.Orientation)
+            if flag then
+                return exifValue.Value
+        } |> Option.defaultValue ExifOrientationMode.TopLeft
+
+    /// Gets the target size for the given target height,
+    /// accounting for rotation, if necessary.
+    let getTargetSize targetHeight imageInfo =
+        match getOrientation imageInfo with
+            | ExifOrientationMode.LeftTop
+            | ExifOrientationMode.RightTop
+            | ExifOrientationMode.RightBottom
+            | ExifOrientationMode.LeftBottom ->
+                Size(targetHeight, 0)   // rotate
+            | _ ->
+                Size(0, targetHeight)
+
+    /// PNG encoder.
+    let private pngEncoder =
+        PngEncoder(
+            CompressionLevel =
+                PngCompressionLevel.NoCompression)
+
+    /// Loads an image from the given file.
+    let private loadImageImpl options (file : FileInfo) =
+
+        use stream = new MemoryStream()
+        do
+                // load image
+            use image = Image.Load(options, file.FullName)
+            image.Mutate(_.AutoOrient() >> ignore)
+
+                // re-encode image to stream
+            let encoder =
+                match image.Metadata.DecodedImageFormat.Name.ToLower() with
+                    | "tiff" -> pngEncoder :> IImageEncoder
+                    | _ ->
+                        image.Configuration
+                            .ImageFormatsManager
+                            .GetEncoder(
+                                image.Metadata.DecodedImageFormat)
+            image.Save(stream, encoder)
+            stream.Position <- 0
+
+            // create Avalonia bitmap
+        new Bitmap(stream)
+
+    /// Loads an image from the given file.
+    let loadImage file =
+        let options = DecoderOptions()
+        loadImageImpl options file
+
+    /// Loads a thumbnail image from the given file.
+    let loadThumbnail height (file : FileInfo) =
+        let imageInfo = Image.Identify file.FullName
+        let options =
+            DecoderOptions(
+                TargetSize =   // decode to the given height
+                    getTargetSize height imageInfo)
+        loadImageImpl options file
