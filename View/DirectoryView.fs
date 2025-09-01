d4896df1e601958996d8682c4896af25156dcbdc
Brian Berns <brianberns@gmail.com>
2025-09-01 13:13:36 -0400

    Reorganized utilties.

--- View/DirectoryView.fs
+++ View/DirectoryView.fs
@@ -6,74 +6,8 @@ open Avalonia.Controls
 open Avalonia.FuncUI
 open Avalonia.FuncUI.DSL
 open Avalonia.FuncUI.Types
-open Avalonia.Interactivity
 open Avalonia.Layout
 open Avalonia.Media
-open Avalonia.Platform.Storage
-
-module Color =
-
-    /// Dark gray.
-    let darkGray = Color.Parse "#181818"
-
-module Button =
-
-    /// Button height and width.
-    let buttonSize = 50
-
-    /// Creates a text button.
-    let createTextImpl text (tooltip : string) enabled onClick =
-        Button.create [
-            Button.content (
-                Viewbox.create [
-                    Viewbox.stretch Stretch.Uniform
-                    Viewbox.stretchDirection StretchDirection.Both
-                    Viewbox.child (
-                        TextBlock.create [
-                            TextBlock.text text
-                            TextBlock.horizontalAlignment HorizontalAlignment.Center
-                            TextBlock.verticalAlignment VerticalAlignment.Center
-                            TextBlock.textWrapping TextWrapping.NoWrap
-                        ]
-                    )
-                ]
-            )
-            Button.tip tooltip
-            Button.isEnabled enabled
-            Button.height buttonSize
-            Button.minWidth buttonSize
-            Button.horizontalAlignment HorizontalAlignment.Stretch
-            Button.verticalAlignment VerticalAlignment.Stretch
-            Button.horizontalContentAlignment HorizontalAlignment.Center
-            Button.verticalContentAlignment VerticalAlignment.Center
-            Button.onClick onClick
-        ]
-
-    /// Creates a text button.
-    let createText text tooltip onClick =
-        createTextImpl text tooltip true onClick
-
-module FileSystemView =
-
-    /// Allows user to select an image.
-    let onSelectImage dispatch args =
-        let topLevel =
-            (args : RoutedEventArgs).Source
-                :?> Control
-                |> TopLevel.GetTopLevel
-        async {
-            let! folders =
-                let options = FilePickerOpenOptions()
-                topLevel
-                    .StorageProvider
-                    .OpenFilePickerAsync(options)
-                    |> Async.AwaitTask
-            if folders.Count > 0 then
-                folders[0].Path.LocalPath
-                    |> FileInfo
-                    |> LoadImage
-                    |> dispatch
-        } |> Async.StartImmediate
 
 module DirectoryView =
 
