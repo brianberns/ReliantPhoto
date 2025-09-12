ea243a7595b0f89cbcb4b752869970ade213edc2
Brian Berns <brianberns@gmail.com>
2025-09-12 10:07:33 -0400

    Refactored.

--- ReliantPhotoViewer/View/View.fs
+++ ReliantPhotoViewer/View/View.fs
@@ -7,72 +7,81 @@ open Avalonia.Input
 
 module View =
 
-    /// Triggers the given message on the given key presses.
-    let private createBindings dispatch keys message =
-        [
-            for key in keys do
-                KeyBinding.create [
-                    KeyBinding.key key
-                    KeyBinding.execute (fun _ ->
-                        message
-                            |> MkImageMessage
-                            |> dispatch)
-                ]
-        ]
+    /// Creates a mode-specific view.
+    let private createModeView model dispatch =
+        match model with
+            | DirectoryMode (dirModel, _) ->
+                DirectoryView.view dirModel dispatch
+                    :> IView
+            | ImageMode (_, imgModel) ->
+                ImageView.view imgModel dispatch
 
-    /// Triggers a result message on the given key presses.
-    let private createResultBindings dispatch keys = function
-        | Some ((file, result) : FileImageResult) ->
-            result
-                |> ImageMessage.ofResult file
-                |> createBindings dispatch keys
-        | None -> []
+    /// Creates key bindings.
+    let private createKeyBindings model dispatch =
+
+        /// Triggers the given message on the given key presses.
+        let createBindings keys message =
+            [
+                for key in keys do
+                    KeyBinding.create [
+                        KeyBinding.key key
+                        KeyBinding.execute (fun _ ->
+                            message
+                                |> MkImageMessage
+                                |> dispatch)
+                    ]
+            ]
+
+        /// Triggers a result message on the given key presses.
+        let createResultBindings keys = function
+            | Some ((file, result) : FileImageResult) ->
+                result
+                    |> ImageMessage.ofResult file
+                    |> createBindings keys
+            | None -> []
+
+        [
+            match model with
+                | ImageMode (_, Situation_ situation) ->
+
+                        // previous image
+                    yield! createResultBindings
+                        [ Key.Left; Key.PageUp ]
+                        situation.PreviousResultOpt
+
+                        // next image
+                    yield! createResultBindings
+                        [ Key.Right; Key.PageDown ]
+                        situation.NextResultOpt
+
+                        // delete file
+                    yield! createBindings
+                        [ Key.Delete ]
+                        DeleteFile
+
+                        // full-screen on
+                    yield! createBindings
+                        [ Key.F11 ]
+                        (FullScreen true)
+
+                        // full-screen off
+                    yield! createBindings
+                        [ Key.Escape ]
+                        (FullScreen false)
+
+                | _ -> ()
+        ]
 
     /// Creates a view of the given model.
     let view model dispatch =
 
         Window.create [
 
-                // mode-specific view
+                // directory vs. image mode
             Window.child (
-                match model with
-                    | DirectoryMode (dirModel, _) ->
-                        DirectoryView.view dirModel dispatch
-                            :> IView
-                    | ImageMode (_, imgModel) ->
-                        ImageView.view imgModel dispatch
-            )
+                createModeView model dispatch)
 
                 // key bindings
-            Window.keyBindings [
-                match model with
-                    | ImageMode (_, Situation_ situation) ->
-
-                            // previous image
-                        yield! createResultBindings dispatch
-                            [ Key.Left; Key.PageUp ]
-                            situation.PreviousResultOpt
-
-                            // next image
-                        yield! createResultBindings dispatch
-                            [ Key.Right; Key.PageDown ]
-                            situation.NextResultOpt
-
-                            // delete file
-                        yield! createBindings dispatch
-                            [ Key.Delete ]
-                            DeleteFile
-
-                            // full-screen on
-                        yield! createBindings dispatch
-                            [ Key.F11 ]
-                            (FullScreen true)
-
-                            // full-screen off
-                        yield! createBindings dispatch
-                            [ Key.Escape ]
-                            (FullScreen false)
-
-                    | _ -> ()
-            ]
+            Window.keyBindings (
+                createKeyBindings model dispatch)
         ]
