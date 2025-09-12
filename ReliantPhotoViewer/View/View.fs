4db83f74b6693a017ee1675c75366906201cc525
Brian Berns <brianberns@gmail.com>
2025-09-12 10:02:15 -0400

    Polish.

--- ReliantPhotoViewer/View/View.fs
+++ ReliantPhotoViewer/View/View.fs
@@ -7,30 +7,33 @@ open Avalonia.Input
 
 module View =
 
+    /// Triggers the given message on the given key presses.
+    let private createBindings dispatch keys message =
+        [
+            for key in keys do
+                KeyBinding.create [
+                    KeyBinding.key key
+                    KeyBinding.execute (fun _ ->
+                        message
+                            |> MkImageMessage
+                            |> dispatch)
+                ]
+        ]
+
+    /// Triggers a result message on the given key presses.
+    let private createResultBindings dispatch keys = function
+        | Some ((file, result) : FileImageResult) ->
+            result
+                |> ImageMessage.ofResult file
+                |> createBindings dispatch keys
+        | None -> []
+
     /// Creates a view of the given model.
     let view model dispatch =
 
-        let createBindings keys message =
-            [
-                for key in keys do
-                    KeyBinding.create [
-                        KeyBinding.key key
-                        KeyBinding.execute (fun _ ->
-                            message
-                                |> MkImageMessage
-                                |> dispatch)
-                    ]
-            ]
-
-        let createResultBindings keys = function
-            | Some ((file, result) : FileImageResult) ->
-                result
-                    |> ImageMessage.ofResult file
-                    |> createBindings keys
-            | None -> []
-
         Window.create [
 
+                // mode-specific view
             Window.child (
                 match model with
                     | DirectoryMode (dirModel, _) ->
@@ -40,32 +43,33 @@ module View =
                         ImageView.view imgModel dispatch
             )
 
+                // key bindings
             Window.keyBindings [
                 match model with
                     | ImageMode (_, Situation_ situation) ->
 
                             // previous image
-                        yield! createResultBindings
+                        yield! createResultBindings dispatch
                             [ Key.Left; Key.PageUp ]
                             situation.PreviousResultOpt
 
                             // next image
-                        yield! createResultBindings
+                        yield! createResultBindings dispatch
                             [ Key.Right; Key.PageDown ]
                             situation.NextResultOpt
 
                             // delete file
-                        yield! createBindings
+                        yield! createBindings dispatch
                             [ Key.Delete ]
                             DeleteFile
 
                             // full-screen on
-                        yield! createBindings
+                        yield! createBindings dispatch
                             [ Key.F11 ]
                             (FullScreen true)
 
                             // full-screen off
-                        yield! createBindings
+                        yield! createBindings dispatch
                             [ Key.Escape ]
                             (FullScreen false)
 
