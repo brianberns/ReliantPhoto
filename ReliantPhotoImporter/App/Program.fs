c333ccbdae6cc99236ce06ed7947284a010d583e
Brian Berns <brianberns@gmail.com>
2025-09-15 00:37:20 -0400

    Began work on importer.

---
+++ ReliantPhotoImporter/App/Program.fs
@@ -0,0 +1,44 @@
+﻿namespace Reliant.Photo
+
+open Avalonia
+open Avalonia.Controls.ApplicationLifetimes
+open Avalonia.Styling
+open Avalonia.Themes.Fluent
+
+type App() =
+    inherit Application(
+        RequestedThemeVariant = ThemeVariant.Dark)
+
+    override this.Initialize() =
+        this.Styles.Add(FluentTheme())
+
+    override this.OnFrameworkInitializationCompleted() =
+        match this.ApplicationLifetime with
+            | :? IClassicDesktopStyleApplicationLifetime as lifetime ->
+                lifetime.MainWindow <- MainWindow(lifetime.Args)
+            | _ -> ()
+
+module Program =
+
+    /// Maximum display string length.
+    let private maxStringLength = 2000
+
+    /// Shortens the given string, if necessary.
+    let private shorten (str : string) =
+        let ellipsis = "…"
+        if str.Length > maxStringLength then
+            str.Substring(0, maxStringLength - ellipsis.Length)
+                + ellipsis
+        else str
+
+    [<EntryPoint>]
+    let main args =
+        try
+            AppBuilder
+                .Configure<App>()
+                .UsePlatformDetect()
+                .UseSkia()
+                .StartWithClassicDesktopLifetime(args)
+        with exn ->
+            printfn $"{exn}"
+            1
---
+++ ReliantPhotoImporter/View/Utility.fs
@@ -0,0 +1,12 @@
+﻿namespace Reliant.Photo
+
+open System.Reflection
+
+module Resource =
+
+    /// Gets a resource by name.
+    let get name =
+        Assembly
+            .GetExecutingAssembly()
+            .GetManifestResourceStream(
+                $"ReliantPhotoImporter.Assets.{name : string}")
