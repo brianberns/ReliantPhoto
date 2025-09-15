64890497f7bc7d3c575b3ecd2dc2c8ce7ab7b982
Brian Berns <brianberns@gmail.com>
2025-09-15 00:37:20 -0400

    Began work on importer.

---
+++ ReliantPhotoImporter/App/MainWindow.fs
@@ -0,0 +1,43 @@
+namespace Reliant.Photo
+
+open Elmish
+
+open Avalonia.Controls
+open Avalonia.FuncUI.Elmish
+open Avalonia.FuncUI.Hosts
+
+module Window =
+
+    /// Gets the window icon.
+    let getIcon () =
+        Resource.get "ReliantPhoto.png"
+            |> WindowIcon
+
+    /// Creates Elmish program.
+    let private makeProgram dpiScale =
+
+        let init = fun () -> ()
+        let update = fun () () -> ()
+        let view () dispatch = Avalonia.FuncUI.DSL.StackPanel.create []
+
+        Program.mkSimple init update view
+
+    /// Starts the Elmish MVU loop.
+    let run (window : HostWindow) arg =
+        makeProgram window.RenderScaling
+            |> Program.withHost window
+#if DEBUG
+            |> Program.withConsoleTrace
+#endif
+            |> Program.runWithAvaloniaSyncDispatch arg
+
+/// Main window.
+type MainWindow(args : string[]) as this =
+    inherit HostWindow(
+        Title = "Reliant Photo Importer",
+        Icon = Window.getIcon (),
+        Width = 600.0,
+        Height = 400.0)
+    do
+        Window.run this ()
+
---
+++ ReliantPhotoImporter/App/Program.fs
@@ -0,0 +1,33 @@
+﻿namespace Reliant.Photo
+
+open Avalonia
+open Avalonia.Controls.ApplicationLifetimes
+open Avalonia.Styling
+open Avalonia.Themes.Fluent
+
+type App() =
+    inherit Application(
+        RequestedThemeVariant = ThemeVariant.Dark)
+
+    override this.Initialize() =
+        this.Styles.Add(FluentTheme())
+
+    override this.OnFrameworkInitializationCompleted() =
+        match this.ApplicationLifetime with
+            | :? IClassicDesktopStyleApplicationLifetime as lifetime ->
+                lifetime.MainWindow <- MainWindow(lifetime.Args)
+            | _ -> ()
+
+module Program =
+
+    [<EntryPoint>]
+    let main args =
+        try
+            AppBuilder
+                .Configure<App>()
+                .UsePlatformDetect()
+                .UseSkia()
+                .StartWithClassicDesktopLifetime(args)
+        with exn ->
+            printfn $"{exn}"
+            1
