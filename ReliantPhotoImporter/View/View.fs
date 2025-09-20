54f53624be55fd477804492f35f0fcaa9a2912c1
Brian Berns <brianberns@gmail.com>
2025-09-19 23:42:45 -0400

    More work on view.

--- ReliantPhotoImporter/View/View.fs
+++ ReliantPhotoImporter/View/View.fs
@@ -1,8 +1,31 @@
 ﻿namespace Reliant.Photo
 
+open Avalonia.Controls
 open Avalonia.FuncUI.DSL
+open Avalonia.Layout
 
 module View =
 
     let view model dispatch =
-        Window.create []
+        Window.create [
+            Window.child (
+                StackPanel.create [
+                    StackPanel.children [
+                        StackPanel.create [
+                            StackPanel.orientation Orientation.Horizontal
+                            StackPanel.children [
+                                TextBlock.create [
+                                    TextBlock.text "Import images to:"
+                                ]
+                                TextBlock.create [
+                                    TextBlock.text "Folder"
+                                ]
+                                Button.create [
+                                    Button.content "Browse"
+                                ]
+                            ]
+                        ]
+                    ]
+                ]
+            )
+        ]
