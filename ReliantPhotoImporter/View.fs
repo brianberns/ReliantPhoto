283cb40efde471044da35f0a8838dae1f1618594
Brian Berns <brianberns@gmail.com>
2025-09-22 21:41:26 -0400

    Cleanup.

--- ReliantPhotoImporter/View.fs
+++ ReliantPhotoImporter/View.fs
@@ -1,7 +1,5 @@
 ﻿namespace Reliant.Photo
 
-#nowarn 57
-
 open System
 open System.IO
 
@@ -10,50 +8,11 @@ open Avalonia.FuncUI
 open Avalonia.FuncUI.DSL
 open Avalonia.FuncUI.Types
 open Avalonia.Layout
-open Avalonia.Media
-open Avalonia.Media.Imaging
 
 module Asset =
 
     /// Asset path.
     let path = "ReliantPhotoImporter.Assets"
-
-module Icon =
-
-    /// Creates an icon.
-    let private create name =
-        let stream =
-            $"{name}_48dp_E3E3E3_FILL0_wght400_GRAD0_opsz48.png"
-                |> Resource.get Asset.path
-        new Bitmap(stream)
-
-    /// Folder open.
-    let folderOpen = create "folder_open"
-
-module Button =
-
-    /// Button height and width.
-    let buttonSize = 42
-
-    /// Creates an icon button.
-    let createIcon icon attrs onClick =
-        Button.create [
-            Button.content (
-                Image.create [
-                    Image.source icon
-                    Image.stretch Stretch.Uniform
-                ]
-            )
-            Button.height buttonSize
-            Button.width buttonSize
-            Button.horizontalAlignment HorizontalAlignment.Left
-            Button.verticalAlignment VerticalAlignment.Center
-            Button.horizontalContentAlignment HorizontalAlignment.Center
-            Button.verticalContentAlignment VerticalAlignment.Center
-            Button.cornerRadius 4.0
-            yield! attrs
-            Button.onClick onClick
-        ]
 
 module View =
 
